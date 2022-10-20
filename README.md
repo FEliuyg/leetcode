f6c9eb0388b8fe11e14b62b2e5ec618f573ccd14
xiaoyao <15558110117@163.com>
2022-10-20 12:50:56 +0800

    fix: update readme and rename 577 to 557

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # leetcode
-leetcode刷题
+
+leetcode 刷题
+
+## 字符串
+
+- [x] 557.反转字符串 III
+- [x] 696.计数二进制子串
--- test/557.test.ts
+++ test/557.test.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "vitest";
-import reverseWords from "../src/577";
+import reverseWords from "../src/557";
 
-test("577.反转字符串", () => {
+test("557.反转字符串", () => {
   expect(reverseWords("Let's take LeetCode contest")).toBe(
     "s'teL ekat edoCteeL tsetnoc"
   );
